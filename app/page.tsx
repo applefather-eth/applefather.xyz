b8625e261d666eb44e70aebc38f50502662e9abb
applefather-eth <huangv.eth@gmail.com>
2025-05-10 15:08:16 +0700

    add simple

--- app/page.tsx
+++ app/page.tsx
@@ -137,8 +137,13 @@ export default function Personal() {
       >
         <div className="flex-1">
           <p className="text-zinc-600 dark:text-zinc-400">
-            Focused on creating intuitive and performant web experiences.
-            Bridging the gap between design and development.
+            I love building things and making them useful for everyone — just
+            like being a good father.
+          </p>
+
+          <p className="text-zinc-600 dark:text-zinc-400">
+            Steve Jobs has always inspired me. That’s why I named my son Apple,
+            so I can be an applefather.
           </p>
         </div>
       </motion.section>
